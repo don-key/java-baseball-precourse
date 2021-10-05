637d22327341c04e8ce83626eb81f0dc32237d9d
donkey <rlaehdzlsla@gmail.com>
2021-10-05 23:29:15 +0900

    refactor(Player): 숫자 생성 방식 수정

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Player {
 
+    private static final int NUMBER_SIZE = 3;
     private final InputView inputView;
     private final List<Integer> numbers = new ArrayList<>();
 
@@ -16,36 +17,43 @@ public class Player {
 
     public void inputNumbers() {
         String stringNumbers = inputView.inputPlayerNumbers();
-        if(stringNumbers.length() != 3) {
-            //TODO ERROR 메세지 추가
-            return;
-        }
-
-        for (int i = 0; i < 3; i++) {
-            addNumber(Character.getNumericValue(stringNumbers.charAt(i)));
-        }
+        addNumber(stringNumbers);
     }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
 
-    private void addNumber(int number) {
-        if(!validateNumber(number)){
+    private void addNumber(String stringNumbers) {
+        validateString(stringNumbers);
+        for (int i = 0; i < NUMBER_SIZE; i++) {
+            int number = Character.getNumericValue(stringNumbers.charAt(i));
+            validateNumber(number);
             numbers.add(number);
         }
     }
 
-    private boolean validateNumber(int number) {
-        return isNumber(number) && !isDuplicate(number);
+    private void validateString(String stringNumber) {
+        checkLength(stringNumber);
+    }
 
+    private void checkLength(String stringNumber) {
+        if (stringNumber.length() != NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumber(int number) {
+        if(!isNumber(number) && isDuplicate(number)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private boolean isNumber(int number) {
         return (0 < number && number < 10);
     }
 
-    private Boolean isDuplicate(int number) {
+    private boolean isDuplicate(int number) {
         return numbers.contains(number);
     }
 
